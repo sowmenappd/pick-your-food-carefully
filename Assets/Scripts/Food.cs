41e817755c8b4fba92f4fe4134fe9f325b24f8ff
Sowmen Rahman <sowmenappd@users.noreply.github.com>
2018-09-28 20:15:03 +0600

    added plate and food objects

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Food : MonoBehaviour {
+	public enum Rarity{ common, special, rare, very_rare, legendary };
+	public enum Effect{ good, bad, poison };
+	new public string name;
+	public int id;
+	public Rarity rarity;
+	public Effect effect;
+	public Sprite sprite;
+	public int healthPoints = 0;
+
+	public event System.Action<int> OnFoodPickedUp, OnFoodMissed;
+
+
+	public void Start(){
+		GetComponent<Image>().sprite = sprite;
+	}
+
+
+	void OnTriggerEnter2D(BoxCollider2D col){
+		if(col.tag == "Player") {
+			if(OnFoodPickedUp != null){
+				OnFoodPickedUp(col.GetComponent<Food>().id);
+			}
+
+		} else if(col.tag == "LowerBarrier") {
+			if(OnFoodMissed != null){
+				OnFoodMissed(col.GetComponent<Food>().id);
+			}
+
+		}
+	}
+}
---
+++ Assets/Scripts/Plate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate : MonoBehaviour {
+
+	[Range(0f, 210f)]
+	public float speed;
+
+	public Transform leftBarrier, rightBarrier;
+
+	void Update () {
+		Vector2 dir = Vector2.right * Input.GetAxisRaw("Horizontal") * speed; 
+		float xPos = Mathf.Clamp(transform.position.x + dir.x, leftBarrier.position.x, rightBarrier.position.x);
+		transform.position = new Vector3(xPos, transform.position.y, transform.position.z);
+	}
+}
